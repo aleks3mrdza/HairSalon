70ec63042eda45f6b45a8d73bc125e2370842902
Danilo Sekulic <Danilo-92-@hotmail.com>
2020-12-29 20:24:51 +0100

    Add Account controller

---
+++ HairSalon.API/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using HairSalon.Models.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HairSalon.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        [Route("login")]
+        public bool Login([FromBody]User user)
+        {
+            return true;
+        }
+
+        [HttpPost]
+        [Route("logout")]
+        public bool Logout([FromBody]User user)
+        {
+            return true;
+        }
+    }
+}
