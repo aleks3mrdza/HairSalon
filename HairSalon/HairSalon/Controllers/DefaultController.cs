9c9ac1421a0cc37d00116a01178055e4ea62c5d8
Danilo Sekulic <danilo.sekulic@htecgroup.com>
2021-09-13 21:00:59 +0200

    Add initial project

---
+++ HairSalon/HairSalon/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HairSalon.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DefaultController : ControllerBase
+    {
+        private readonly ILogger<DefaultController> _logger;
+
+        public DefaultController(ILogger<DefaultController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
