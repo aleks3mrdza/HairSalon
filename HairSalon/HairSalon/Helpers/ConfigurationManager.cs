68efa12686eba77f95531a856ea2ac84c507e9d6
Danilo Sekulic <danilo.sekulic@htecgroup.com>
2021-09-30 22:23:20 +0200

    Use configuration for database properties

---
+++ HairSalon/HairSalon/Helpers/ConfigurationManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace HairSalon.Helpers
+{
+    public class ConfigurationManager
+    {
+        public static IConfiguration appSettings { get; }
+
+        static ConfigurationManager()
+        {
+            appSettings = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+        }
+
+        public static string GetSetting(string name)
+        {
+            return appSettings[name];
+        }
+    }
+}
