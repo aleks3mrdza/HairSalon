9503910ff1b18f26f847178305c3c367640af396
aleks3mrdza <aleks3mrdza@hotmail.com>
2021-10-12 23:00:54 +0200

    Add Salon Controller and salon model

---
+++ HairSalon/HairSalon/Controllers/SalonController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HairSalon.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SalonController : ControllerBase
+    {
+        private readonly ILogger<SalonController> _logger;
+
+        public SalonController(ILogger<SalonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetSalon()
+        {
+
+            var salons = new List<Salon>();
+
+            var salon = new Salon();
+            salon.Name = "MuhiStil";
+            salon.Address = "Kosancic Ivana 50";
+            salon.Place = "Vrsac";
+
+            return Ok(salon);
+
+        }
+    }
+}
---
+++ HairSalon/HairSalon/Models/Salon.cs
@@ -0,0 +1,11 @@
+﻿namespace HairSalon.Models
+{
+    public class Salon
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Place { get; set; }
+    }
+}
